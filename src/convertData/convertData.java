0838f333167d1b70d36a4bf0999450b33e1749ef
LinkedFluuuush <linkedfluuuush@gmail.com>
2013-11-13 01:08:53 +0100

    End conversion files

--- src/convertData/convertData.java
+++ src/convertData/convertData.java
@@ -1,7 +1,6 @@
 package convertData;
 
 import com.sun.org.apache.xalan.internal.xsltc.runtime.BasisLibrary;
-import com.sun.org.apache.xalan.internal.xsltc.runtime.BasisLibrary.stringToInt;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.output.Format;
@@ -27,18 +26,24 @@ public class convertData{
             if(files[i].getName().equals("Armes")){
                 convertWeapons(files[i]);
                 System.out.println("Conversion des armes effectuée.");
-            /*} else if(files[i].getName() == "Armures"){
-
-            } else if(files[i].getName() == "Competences"){
-
-            } else if(files[i].getName() == "Objets"){
-
-            } else if(files[i].getName() == "Talents"){
-
-            } else if(files[i].getName() == "Carrieres"){
-
-            } else if(files[i].getName() == "Races"){
-*/
+            } else if(files[i].getName().equals("Armures")){
+                convertArmours(files[i]);
+                System.out.println("Conversion des armures effectuée.");
+            } else if(files[i].getName().equals("Competences")){
+                convertSkills(files[i]);
+                System.out.println("Conversion des compétences effectuée.");
+            } else if(files[i].getName().equals("Objets")){
+                convertEquipment(files[i]);
+                System.out.println("Conversion des objets effectuée.");
+            } else if(files[i].getName().equals("Talents")){
+                convertTalents(files[i]);
+                System.out.println("Conversion des talents effectuée.");
+            } else if(files[i].getName().equals("Carrieres")){
+                convertCareers(files[i]);
+                System.out.println("Conversion des carrières effectuée.");
+            } else if(files[i].getName().equals("Races")){
+                convertRace(files[i]);
+                System.out.println("Conversion des races effectuée.");
             } else {
                 System.out.println("Fichier non reconnu : " + files[i].getName() + ".");
             }
@@ -169,17 +174,408 @@ public class convertData{
                 equipment.setAttribute("name", s1[0]);
                 equipment.setAttribute("enc", s1[1]);
 
-                int[] price = convertPrices(BasisLibrary.stringToInt())
+                int[] price = convertPrices(BasisLibrary.stringToInt(s1[2]));
 
-                liO.add(new Objet(s.substring(0,n),stringToInt(s.substring(n+1, n1)), stringToInt(s.substring(n1+1))));
+                equipment.setAttribute("goldenCrowns", ""+price[0]);
+                equipment.setAttribute("silverShillings", ""+price[1]);
+                equipment.setAttribute("brassPennies", ""+price[2]);
+
+                root.addContent(equipment);
             }
 
             br.close();
+
+            XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
+            xmlOutputter.output(document, new FileOutputStream("resources/equipment.xml"));
         } catch (FileNotFoundException e) {
         } catch (IOException e) {
         }
     }
 
+    public static void convertSkills(File file){
+            System.out.println("Traitement de : " + file.getName());
+
+            Element root = new Element("skills");
+            Document document = new Document(root);
+            Element skill;
+
+            String s;
+            int n;
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                while(br.ready()){
+                    s = br.readLine();
+                    n = s.lastIndexOf(' ');
+
+                    skill = new Element("skill");
+
+                    skill.setAttribute("name", s.substring(0, n));
+                    skill.setAttribute("competence", s.substring(n+1));
+
+                    root.addContent(skill);
+                }
+
+                br.close();
+
+            XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
+            xmlOutputter.output(document, new FileOutputStream("resources/skills.xml"));
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }
+    }
+
+    public static void convertTalents(File file){
+        System.out.println("Traitement de : " + file.getName());
+
+        Element root = new Element("talents");
+        Document document = new Document(root);
+        Element talent;
+
+        String s;
+        String[] s1;
+        int n;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            while(br.ready()){
+                s = br.readLine();
+                s1 = s.split(":");
+
+                talent = new Element("talent");
+
+                talent.setAttribute("name", s1[0]);
+                talent.setAttribute("description", s1[1]);
+
+                root.addContent(talent);
+            }
+
+            br.close();
+
+            XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
+            xmlOutputter.output(document, new FileOutputStream("resources/talents.xml"));
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }
+    }
+
+    public static void convertRace(File file){
+        File[] raceFiles = file.listFiles();
+        Element root = new Element("races");
+        Document document = new Document(root);
+        Element race;
+        String line;
+        String[] elts;
+
+        for(File f : raceFiles){
+            System.out.println("Traitement de : " + f.getName());
+
+            race = new Element("race");
+            race.setAttribute("name", f.getName());
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(f));
+
+                line = br.readLine();
+                elts = line.split(",");
+
+                Element profile = new Element("profile");
+
+                profile.setAttribute("WS", elts[0]);
+                profile.setAttribute("BS", elts[1]);
+                profile.setAttribute("S", elts[2]);
+                profile.setAttribute("T", elts[3]);
+                profile.setAttribute("Ag", elts[4]);
+                profile.setAttribute("Int", elts[5]);
+                profile.setAttribute("WP", elts[6]);
+                profile.setAttribute("Fel", elts[7]);
+                profile.setAttribute("M", elts[8]);
+
+                race.addContent(profile);
+
+                Element size = new Element("size");
+                size.setAttribute("F", elts[9]);
+                size.setAttribute("M", elts[10]);
+
+                race.addContent(size);
+
+                line = br.readLine();
+                elts = line.split(",");
+
+                Element wounds = new Element("wounds");
+
+                for(int i = 0; i < elts.length ; i++){
+                    Element choice = new Element("choice");
+                    choice.setText(elts[i]);
+                    wounds.addContent(choice);
+                }
+
+                race.addContent(wounds);
+
+                line = br.readLine();
+                elts = line.split(",");
+
+                Element fate = new Element("fate");
+
+                for(int i = 0; i < elts.length ; i++){
+                    Element choice = new Element("choice");
+                    choice.setText(elts[i]);
+                    fate.addContent(choice);
+                }
+                race.addContent(fate);
+
+                line = br.readLine();
+                elts = line.split(",");
+
+                Element names = new Element("fNames");
+
+                for(String name : elts){
+                    Element eName = new Element("name");
+                    eName.setText(name);
+                    names.addContent(eName);
+                }
+
+                race.addContent(names);
+
+                line = br.readLine();
+                elts = line.split(",");
+
+                names = new Element("mNames");
+
+                for(String name : elts){
+                    Element eName = new Element("name");
+                    eName.setText(name);
+                    names.addContent(eName);
+                }
+
+                race.addContent(names);
+
+                line = br.readLine();
+                elts = line.split(",");
+
+                Element hair = new Element("hairColour");
+
+                for(String colour : elts){
+                    Element eColour = new Element("colour");
+                    eColour.setText(colour);
+                    hair.addContent(eColour);
+                }
+
+                race.addContent(hair);
+
+                line = br.readLine();
+                elts = line.split(",");
+
+                Element eyes = new Element("eyeColour");
+
+                for(String colour : elts){
+                    Element eColour = new Element("colour");
+                    eColour.setText(colour);
+                    eyes.addContent(eColour);
+                }
+
+                race.addContent(eyes);
+
+                line = br.readLine();
+                elts = line.split(",");
+
+                Element weightTable = new Element("weightTable");
+
+                for(String weight : elts){
+                    Element eWeight = new Element("eWeight");
+                    eWeight.setText(weight);
+                    weightTable.addContent(eWeight);
+                }
+
+                race.addContent(weightTable);
+
+                line = br.readLine();
+                elts = line.split(",");
+
+                Element ageTable = new Element("ageTable");
+
+                for(String age : elts){
+                    Element eAge = new Element("eAge");
+                    eAge.setText(age);
+                    ageTable.addContent(eAge);
+                }
+
+                race.addContent(ageTable);
+
+                line = br.readLine();
+                elts = line.split(",");
+
+                Element competencesTable = new Element("competencesTable");
+
+                for(String competences : elts){
+                    Element eCompetences = new Element("eCompetences");
+
+                    String[] choices = competences.split(":");
+
+                    for(int i = 0 ; i < choices.length ; i++){
+                        Element choice = new Element("choice");
+                        choice.setText(choices[i]);
+                        eCompetences.addContent(choice);
+                    }
+
+                    competencesTable.addContent(eCompetences);
+                }
+
+                race.addContent(competencesTable);
+
+                line = br.readLine();
+                elts = line.split(",");
+
+                Element talentsTable = new Element("talentsTable");
+
+                for(String talents : elts){
+                    Element eTalents = new Element("eTalents");
+
+                    String[] choices = talents.split(":");
+
+                    for(int i = 0 ; i < choices.length ; i++){
+                        Element choice = new Element("choice");
+                        choice.setText(choices[i]);
+                        eTalents.addContent(choice);
+                    }
+
+                    talentsTable.addContent(eTalents);
+                }
+
+                race.addContent(talentsTable);
+
+                root.addContent(race);
+
+                br.close();
+
+            } catch (FileNotFoundException e) {
+            } catch (IOException e) {
+            }
+        }
+
+        try {
+            XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
+            xmlOutputter.output(document, new FileOutputStream("resources/races.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+    }
+
+    public static void convertCareers(File file){
+        File[] careersFiles = file.listFiles();
+        Element root = new Element("careers");
+        Document document = new Document(root);
+        Element career;
+        String line;
+        String[] elts;
+
+        for(File f : careersFiles){
+            System.out.println("Traitement de : " + f.getName());
+
+            career = new Element("career");
+            career.setName(f.getName());
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(f));
+
+                line = br.readLine();
+                elts = line.split(",");
+
+                Element profile = new Element("profile");
+
+                profile.setAttribute("WS", elts[0]);
+                profile.setAttribute("BS", elts[1]);
+                profile.setAttribute("S", elts[2]);
+                profile.setAttribute("T", elts[3]);
+                profile.setAttribute("Ag", elts[4]);
+                profile.setAttribute("Int", elts[5]);
+                profile.setAttribute("WP", elts[6]);
+                profile.setAttribute("Fel", elts[7]);
+                profile.setAttribute("A", elts[8]);
+                profile.setAttribute("W", elts[9]);
+                profile.setAttribute("M", elts[10]);
+                profile.setAttribute("Mag", elts[11]);
+
+                career.addContent(profile);
+
+                line = br.readLine();
+                elts = line.split(",");
+
+                Element competencesTable = new Element("competencesTable");
+
+                for(String competences : elts){
+                    Element eCompetences = new Element("eCompetences");
+
+                    String[] choices = competences.split(":");
+
+                    for(int i = 0 ; i < choices.length ; i++){
+                        Element choice = new Element("choice");
+                        choice.setText(choices[i]);
+                        eCompetences.addContent(choice);
+                    }
+
+                    competencesTable.addContent(eCompetences);
+                }
+
+                career.addContent(competencesTable);
+
+                line = br.readLine();
+                elts = line.split(",");
+
+                Element talentsTable = new Element("talentsTable");
+
+                for(String talents : elts){
+                    Element eTalents = new Element("eTalents");
+
+                    String[] choices = talents.split(":");
+
+                    for(int i = 0 ; i < choices.length ; i++){
+                        Element choice = new Element("choice");
+                        choice.setText(choices[i]);
+                        eTalents.addContent(choice);
+                    }
+
+                    talentsTable.addContent(eTalents);
+                }
+
+                career.addContent(talentsTable);
+
+                line = br.readLine();
+                elts = line.split(",");
+
+                Element trappingsTable = new Element("trappingsTable");
+
+                for(String trapping : elts){
+                    Element eTrappings = new Element("eTrappings");
+
+                    String[] choices = trapping.split(":");
+
+                    for(int i = 0 ; i < choices.length ; i++){
+                        Element choice = new Element("choice");
+                        choice.setText(choices[i]);
+                        eTrappings.addContent(choice);
+                    }
+
+                    talentsTable.addContent(eTrappings);
+                }
+
+                career.addContent(trappingsTable);
+
+                root.addContent(career);
+
+            } catch (FileNotFoundException e) {
+            } catch (IOException e) {
+            }
+        }
+
+        try {
+            XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
+            xmlOutputter.output(document, new FileOutputStream("resources/careers.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
 
 
     public static int[] convertPrices(int price){
@@ -187,7 +583,7 @@ public class convertData{
 
         finalPrice[0] = price / 240;
         finalPrice[1] = (price % 240) / 12;
-        finalPrice[3] = (price % 240) % 12;
+        finalPrice[2] = (price % 240) % 12;
 
         return finalPrice;
     }
